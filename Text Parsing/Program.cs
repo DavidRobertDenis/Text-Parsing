b562e6184286dc92d3cc4adc4b78e0feda2991be
DavidRobertDenis <110538874+DavidRobertDenis@users.noreply.github.com>
2024-01-03 14:07:41 +0200

    Add files via upload

---
+++ Text Parsing/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string City { get; set; }
+
+    public Person(string firstName, string lastName, string city)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        City = city;
+    }
+
+    public void PrintInfo()
+    {
+        Console.WriteLine($"{FirstName} {LastName} {City}");
+    }
+}
+
+class Program
+{
+    private const int v = 0;
+
+    static void Main()
+    {
+        string text = "John.Davidson/Belgrade Michael.Barton/Krakow Ivan.Perkinson/Moscow";
+
+         
+        string[] entries = text.Split(' ');
+
+         
+        Person[] persons = new Person[entries.Length];
+
+        for (int i = 0; i < entries.Length; i++)
+        {
+             
+            string[] parts = entries[i].Split('/');
+
+             
+            persons[i] = new Person(parts[v], parts[1], parts[2]);
+        }
+
+         
+        foreach (Person person in persons)
+        {
+            person.PrintInfo();
+        }
+    }
+}
+
